5edeb27a5b4f7b0b238ef090cd51383b0d4d6c1c
jacksongraves <jackson.graves@me.com>
2022-02-11 22:55:53 -0600

    Set up additional UI components

--- src/components/SelectLocation.tsx
+++ src/components/SelectLocation.tsx
@@ -1,1 +1,58 @@
-export {};
+import React, { useState } from "react";
+import {
+	Button,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Grid,
+	Radio,
+	RadioGroup,
+	TextField,
+	Typography,
+} from "@mui/material";
+import locations from "constants/locations";
+
+const SelectLocation = (): JSX.Element => {
+	const [locationIndex, setLocationIndex] = useState<number>(0);
+	const [keyword, setKeyword] = useState<string>("");
+
+	const handleChangeLocation = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setLocationIndex(Number(event.target.value));
+	};
+
+	const handleChangeKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setKeyword(event.target.value);
+	};
+
+	const updateSearch = async () => {};
+	return (
+		<Grid>
+			<Typography variant="h4">Select a Location</Typography>
+			<FormControl>
+				<FormLabel>Select a Location</FormLabel>
+				<RadioGroup
+					defaultValue="female"
+					value={locationIndex}
+					name="radio-buttons-group"
+					onChange={handleChangeLocation}
+				>
+					{locations.map(({ name, latitude, longitude }, index) => (
+						<FormControlLabel key={index} value={index} control={<Radio />} label={name} />
+					))}
+				</RadioGroup>
+			</FormControl>
+
+			<TextField
+				label="Keyword"
+				value={keyword}
+				onChange={handleChangeKeyword}
+				fullWidth
+				variant="outlined"
+			/>
+
+			<Button variant="contained">Search</Button>
+		</Grid>
+	);
+};
+
+export default SelectLocation;
